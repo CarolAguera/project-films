58b76f7237bb5491749a2ff277b07d9bff6b467e
CarolAguera <carolaguerabr@gmail.com>
2021-12-03 22:13:28 -0300

    Feat: new page Detail Films

--- src/components/CardLancamentos/CardLancamentos.jsx
+++ src/components/CardLancamentos/CardLancamentos.jsx
@@ -1,9 +1,14 @@
 import style from './CardLancamentos.module.scss'
 import Image from 'next/image'
+import Link from 'next/link'
 export default function CardLancamentos(props) {
   return (
     <div className={style['cardLancamento']}>
-      <Image src={props.foto} width="193" height="290"></Image>
+      <Link href="/search">
+        <a>
+          <Image src={props.foto} width="193" height="290"></Image>
+        </a>
+      </Link>
       <div className={style['cardLancamento__text']}>
         <h5>{props.name}</h5>
         <p>{props.year}</p>
